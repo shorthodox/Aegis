199c8d87bd2f23d0e1ca806378975f848ce67ca6
shorthodox <animeshkukreti60@gmail.com>
2026-05-09 14:44:03 +0530

    Update gatekeeper.js

--- web/src/scripts/gatekeeper.js
+++ web/src/scripts/gatekeeper.js
@@ -16,7 +16,7 @@ import {
 const firebaseConfig = {
   apiKey: "AIzaSyDtudUL2sE1_fKbzIro5d2IP0-M2dYI6x4",
   authDomain: "aegis-d78e1.firebaseapp.com",
-  projectId: "aegis-d78e1",
+  projectId: "Aegis",
   storageBucket: "aegis-d78e1.firebasestorage.app",
   messagingSenderId: "623998601232",
   appId: "1:623998601232:web:288a89514d84ac3573a295",
