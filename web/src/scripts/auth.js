08f810c6b07166750cf9bdd6e9fd795d467ed440
shorthodox <animeshkukreti60@gmail.com>
2026-05-09 15:24:29 +0530

    Update auth.js

--- web/src/scripts/auth.js
+++ web/src/scripts/auth.js
@@ -44,6 +44,18 @@ let confirmationResult = null;
 // ============================================================
 export async function ensureUserDocumentV2(user, authMethod = 'email') {
   if (!user) return null;
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtudUL2sE1_fKbzIro5d2IP0-M2dYI6x4",
+  authDomain: "aegis-d78e1.firebaseapp.com",
+  databaseURL: "https://aegis-d78e1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aegis-d78e1",
+  storageBucket: "aegis-d78e1.firebasestorage.app",
+  messagingSenderId: "623998601232",
+  appId: "1:623998601232:web:e084edadb1fef0cd73a295",
+  measurementId: "G-B75WNF0MSK"
+};
   
   const userDocRef = doc(db, 'users', user.uid);
   const docSnap = await getDoc(userDocRef);
