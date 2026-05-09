1e66b39a069d53065ede8dcf41bf3cb105383ca7
shorthodox <animeshkukreti60@gmail.com>
2026-05-10 00:15:02 +0530

    Update gatekeeper.js

--- web/src/scripts/gatekeeper.js
+++ web/src/scripts/gatekeeper.js
@@ -10,14 +10,12 @@ import {
   getAuth, onAuthStateChanged, signOut
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
 
-// -------------------------------------------------------------------
-// Firebase Configuration
-// -------------------------------------------------------------------
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDtudUL2sE1_fKbzIro5d2IP0-M2dYI6x4",
   authDomain: "aegis-d78e1.firebaseapp.com",
   databaseURL: "https://aegis-d78e1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "aegis-d78e1", // This is crucial for Firestore
+  projectId: "aegis-d78e1",
   storageBucket: "aegis-d78e1.firebasestorage.app",
   messagingSenderId: "623998601232",
   appId: "1:623998601232:web:288a89514d84ac3573a295",
